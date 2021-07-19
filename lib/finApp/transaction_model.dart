e4fd4c0f2a0c5160cb6c20bd437cfb1dab1321c5
EduardRo <eduardstancu@yahoo.com>
2021-07-19 18:26:15 +0300

    modif men pe clase

---
+++ lib/finApp/transaction_model.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+class TransactionModel {
+  final String logo;
+  final String name;
+  final String date;
+  final String amount;
+
+  const TransactionModel({
+    this.logo = 'logo',
+    this.name = 'name',
+    this.date = 'date',
+    this.amount = 'amount',
+  });
+}
