05ae4475a1c2060d8d343eec6ea1882bc0d47909
EduardRo <eduardstancu@yahoo.com>
2021-07-28 11:13:36 +0300

    menus model

---
+++ lib/matematicon/models/menus_model.dart
@@ -0,0 +1,25 @@
+class MenusModel {
+  String codclasa;
+  String codmaterie;
+  String codserie;
+  String denumireserie;
+
+  MenusModel({
+    required this.codclasa,
+    required this.codmaterie,
+    required this.codserie,
+    required this.denumireserie,
+  });
+/*
+  static List<IconModel> icons = [
+    IconModel(icon: "assets/icons/icon_1.png", title: "Discover"),
+    IconModel(icon: "assets/icons/icon_2.png", title: "Arbitrary"),
+    IconModel(icon: "assets/icons/icon_3.png", title: "Author Nearby"),
+    IconModel(icon: "assets/icons/icon_4.png", title: "WorldWide"),
+    IconModel(icon: "assets/icons/icon_5.png", title: "Audio Book"),
+    IconModel(icon: "assets/icons/icon_6.png", title: "Quick Search"),
+    IconModel(icon: "assets/icons/icon_7.png", title: "My Country"),
+  ];
+
+*/
+}
